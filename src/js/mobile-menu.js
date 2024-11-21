2f15904994af1eb336b3b63a1ae9e0622f4d7dd6
Mioshanika <miosha.tala@gmail.com>
2024-11-21 09:09:56 +0200

    close menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,7 +1,16 @@
 const menu_button = document.getElementById('burger-btn');
+const menu_close_button = document.getElementById('close-btn');
+
 menu_button.addEventListener('click', event => {
     event.preventDefault();
 
     var mm_backdrop = document.getElementById('mobile-menu-backdrop');
     mm_backdrop.classList.add('is-open');
+});
+
+menu_close_button.addEventListener('click', event => {
+    event.preventDefault();
+
+    var mm_backdrop = document.getElementById('mobile-menu-backdrop');
+    mm_backdrop.classList.remove('is-open');
 });
